8da96ad3dade2f53e2d6c149e1f94160174be107
EnesArslan8 <enes8arslan@gmail.com>
2023-07-30 22:19:32 +0300

    Create Item Category and added html ,binded API

--- src/components/ItemCategory.js
+++ src/components/ItemCategory.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import SubdirectoryArrowRightIcon from "@mui/icons-material/SubdirectoryArrowRight";
+import axios from "axios";
 
 function ItemCategory() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/item-category?offset=20&limit=54"
+        );
+        setData(response.data.results);
+      } catch (err) {
+        console.log("Item category yüklenirken hata oluştu:" + err);
+      }
+    };
+    fecthData();
+  }, []);
+  console.log(data);
   return (
-    <div>ItemCategory</div>
-  )
+    <div>
+      <h1>Item Category</h1>
+      <div className="container">
+        <div className="categoryItems">
+          
+          {data.map((item,id)=>{
+            return <span key={id} className="categoryText"><SubdirectoryArrowRightIcon />{item.name}</span>
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ItemCategory
+export default ItemCategory;
